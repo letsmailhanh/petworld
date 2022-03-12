3dce659b54deee418e6754ab9184829d287ec91c
buingochuyen622 <buingochuyen17462@gmail.com>
2022-03-12 18:01:12 +0700

    Add database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -40,20 +40,33 @@ INSERT INTO [Product] VALUES (N'Poodle', 1, 1, '12500000', 1, N'Image\poodle1.jp
 INSERT INTO [Product] VALUES (N'Poodle', 1, 1, '6700000', 1, N'Image\poodle1.jpg')
 INSERT INTO [Product] VALUES (N'Poodle', 1, 1, '5300000', 1, N'Image\poodle1.jpg')
 
-INSERT INTO [Product] VALUES (N'Maine coon', 2, 1, , 1, )
-INSERT INTO [Product] VALUES (N'Anh lông ngắn', 2, 1, , 1, )
-INSERT INTO [Product] VALUES (N'Anh lông dài', 2, 1, , 1, )
-INSERT INTO [Product] VALUES (N'Ba tư', 2, 1, , 1, )
-INSERT INTO [Product] VALUES (N'Bò sát', 3, 1, , 1, )
-INSERT INTO [Product] VALUES (N'Cá', 3, 1, , 1, )
-INSERT INTO [Product] VALUES (N'Hamster', 3, 1, , 1, )
+INSERT INTO [Product] VALUES (N'Maine coon', 2, 1, '12500000', 1, N'Image\mainecoon1.jpg')
+INSERT INTO [Product] VALUES (N'Maine coon', 2, 1, '15500000', 1, N'Image\mainecoon2.jpg')
+
+INSERT INTO [Product] VALUES (N'Anh lông ngắn', 2, 1, '16000000', 1, N'Image\anhngan1.jpg')
+INSERT INTO [Product] VALUES (N'Anh lông ngắn', 2, 1, '13300000', 1, N'Image\anhngan2.jpg')
+
+INSERT INTO [Product] VALUES (N'Anh lông dài', 2, 1, '6700000', 1, N'Image\anhdai1.jpg')
+
+INSERT INTO [Product] VALUES (N'Ba tư', 2, 1, '12800000', 1, N'Image\batu1.jpg')
+
+INSERT INTO [Product] VALUES (N'Bò sát', 3, 1, '320000', 1, N'Image\bosat1.jpg')
+INSERT INTO [Product] VALUES (N'Bò sát', 3, 1, '3500000', 1, N'Image\bosat2.jpg')
+INSERT INTO [Product] VALUES (N'Bò sát', 3, 1, '800000', 1, N'Image\bosat3.jpg')
+
+INSERT INTO [Product] VALUES (N'Cá', 3, 1, '12500000', 1, N'Image\mainecoon1.jpg')
+INSERT INTO [Product] VALUES (N'Cá', 3, 1, '12500000', 1, N'Image\mainecoon1.jpg')
+INSERT INTO [Product] VALUES (N'Cá', 3, 1, '12500000' , 1, N'Image\mainecoon1.jpg')
+
+INSERT INTO [Product] VALUES (N'Hamster', 3, 1, '125000', 1, N'Image\hamster1.jpg')
+INSERT INTO [Product] VALUES (N'Hamster', 3, 1, '50000', 1, N'Image\hamster2.jpg')
 
 --Insert accessory
-INSERT INTO [Product] VALUES (N'Xương Gặm Orgo Sạch Răng', 4, 0, '20000', 1, N'Image\thucan1.jpg')
-INSERT INTO [Product] VALUES (N'Hạt Smartheart Gold Puppy 1kg', 4, 0, '119000', 1, N'Image\thucan2.jpg')
+INSERT INTO [Product] VALUES (N'Xương Gặm Orgo Sạch Răng', 4, 0, '20000', 20, N'Image\thucan1.jpg')
+INSERT INTO [Product] VALUES (N'Hạt Smartheart Gold Puppy 1kg', 4, 0, '119000', 13, N'Image\thucan2.jpg')
 INSERT INTO [Product] VALUES (N'Hạt mềm Zenith', 4, 0, '450000', 1, N'Image\thucan3.jpg')
 
-INSERT INTO [Product] VALUES (N'Cần câu cho mèo', 8, 0, '35000', 1, N'Image\toy1.jpg')
-INSERT INTO [Product] VALUES (N'Ống cào móng', 8, 0, '60000', 1, N'Image\toy2.jpg')
-INSERT INTO [Product] VALUES (N'Xương cao su', 8, 0, '25000', 1, N'Image\toy3.jpg')
-INSERT INTO [Product] VALUES (N'Đĩa bay vải', 8, 0, '45000', 1, N'Image\toy4.jpg')
+INSERT INTO [Product] VALUES (N'Cần câu cho mèo', 8, 0, '35000', 21, N'Image\toy1.jpg')
+INSERT INTO [Product] VALUES (N'Ống cào móng', 8, 0, '60000', 3, N'Image\toy2.jpg')
+INSERT INTO [Product] VALUES (N'Xương cao su', 8, 0, '25000', 12, N'Image\toy3.jpg')
+INSERT INTO [Product] VALUES (N'Đĩa bay vải', 8, 0, '45000', 7, N'Image\toy4.jpg')
