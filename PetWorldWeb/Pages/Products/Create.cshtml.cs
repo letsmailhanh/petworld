e74869fdb464ca7a902f45d219644516869aab8d
buingochuyen622 <buingochuyen17462@gmail.com>
2022-03-24 10:09:11 +0700

    Update entity framework

---
+++ PetWorldWeb/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DataAccess.Model;
+
+namespace PetWorldWeb.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly prn221_petworldContext _context;
+
+        public CreateModel(prn221_petworldContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Title");
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
